3e98684921dd1325bb2642157b539ac6d403f368
Jiaqi Guan <jiaqi.guan1@bytedance.com>
2025-01-10 08:37:36 +0800

    default ESM false

--- inference_demo.sh
+++ inference_demo.sh
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 export LAYERNORM_TYPE=fast_layernorm
-export USE_DEEPSPEED_EVO_ATTTENTION=false
+export USE_DEEPSPEED_EVO_ATTTENTION=true
 
 N_sample=5
 N_step=200
@@ -21,7 +21,7 @@ N_cycle=10
 seed=101
 use_deepspeed_evo_attention=true
 input_json_path="./examples/example.json"
-dump_dir="./output_esm"
+dump_dir="./output"
 
 python3 runner/inference.py \
 --seeds ${seed} \
@@ -29,4 +29,4 @@ python3 runner/inference.py \
 --input_json_path ${input_json_path} \
 --model.N_cycle ${N_cycle} \
 --sample_diffusion.N_sample ${N_sample} \
---sample_diffusion.N_step ${N_step} --use_msa false
+--sample_diffusion.N_step ${N_step}
