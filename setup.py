f888495500c45f1e4ff6edc4be245345dbde60ab
zhangyuxuann <zhangyuxuan.youison@bytedance.com>
2025-11-27 14:55:55 +0800

    update to v0.7.3 for fixing ref_space_uid

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ if "--cpu" in sys.argv:
 setup(
     name="protenix",
     python_requires=">=3.11",
-    version="0.7.2",
+    version="0.7.3",
     description="A trainable PyTorch reproduction of AlphaFold 3.",
     long_description=long_description,
     long_description_content_type="text/markdown",
